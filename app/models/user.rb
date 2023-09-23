85eb214db8a25acc372f09295f44cd3e04db7af2
sayaka-shinomura <138592145+sayaka-shinomura@users.noreply.github.com>
2023-09-23 03:08:10 +0000

    [userモデル]作成

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  #0が男性、1が女性
+  enum gender: { man: 0, woman: 1 }
+
+end

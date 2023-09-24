9b8117d6de0554b437600f1a93183228a842a8fb
sayaka-shinomura <138592145+sayaka-shinomura@users.noreply.github.com>
2023-09-24 04:58:50 +0000

    [ユーザー編集ボタン]実装

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,5 +1,6 @@
 class Public::UsersController < ApplicationController
   def show
+    @user = current_user
   end
 
   def index
@@ -8,6 +9,31 @@ class Public::UsersController < ApplicationController
   def edit
   end
 
+  def update
+    if @user.update(customer_params)
+      redirect_to mypage_path, notice: '会員情報の更新が完了しました。'
+    else
+      render :edit
+    end
+  end
+
   def check
   end
+
+  def withdraw
+    @user.update(is_active: false)
+    reset_session
+    redirect_to root_path
+  end
+
+  private
+
+  def set_current_user
+    @user = current_user
+  end
+
+  def cuser_params
+    params.require(:user).permit(:account_name, :email, :telephone_number, :birth_date, :gender)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
 
-  namespace :public do
-    get 'users/show'
-    get 'users/index'
-    get 'users/edit'
-    get 'users/check'
-  end
 # ユーザー用
 devise_for :users, controllers: {
   registrations: "public/registrations",
@@ -17,15 +11,22 @@ devise_for :admin, controllers: {
   sessions: "admin/sessions"
 }
 
-namespace :admin do
-    get 'top' => 'homes#top', as: 'top'
-  end
 
 scope module: :public do
     root 'homes#top'
 
     get 'users/mypage' => 'users#show', as: 'mypage'
+    get 'users/information/edit' => 'users#edit', as: 'edit_information'
+    patch 'users/information' => 'users#update', as: 'update_information'
+    get 'users/check' => 'users#check', as: 'check_user'
+    put 'users/information' => 'users#update'
+    patch 'users/withdraw' => 'users#withdraw', as: 'withdraw_user'
 
+end
+
+
+namespace :admin do
+    get 'top' => 'homes#top', as: 'top'
   end
 
 end

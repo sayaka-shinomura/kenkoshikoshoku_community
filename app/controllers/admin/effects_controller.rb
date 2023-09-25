1261aae8ba538feaa0bf51c2a65893f6c90e3671
sayaka-shinomura <138592145+sayaka-shinomura@users.noreply.github.com>
2023-09-25 06:19:45 +0000

    [管理者:効能]登録機能実装

--- app/controllers/admin/effects_controller.rb
+++ app/controllers/admin/effects_controller.rb
@@ -1,13 +1,45 @@
 class Admin::EffectsController < ApplicationController
+
   def index
+    @effects = Effect.all
   end
 
   def new
+    @effect = Effect.new
+  end
+
+  def create
+    @effect = Effect.new(effect_params)
+    if @effect.save
+      redirect_to admin_effect_path(@effect.id)
+    else
+      @effects = Effect.all
+      render :new
+    end
   end
 
   def show
+    @effect = Effect.find(params[:id])
+    @effects = Effect.all
   end
 
   def edit
+    @effect = Effect.find(params[:id])
   end
+
+  def update
+     @effect = Effect.find(params[:id])
+    if @effect.update(effect_params)
+      redirect_to admin_effect_path(@effect.id)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def effect_params
+    params.require(:effect).permit(:name, :introduction)
+  end
+
 end

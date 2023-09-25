1e928f9fbc1262851a5a8097cd5cc261342d2e4d
sayaka-shinomura <138592145+sayaka-shinomura@users.noreply.github.com>
2023-09-25 02:08:52 +0000

    [vegetableモデル]作成

---
+++ app/models/vegetable.rb
@@ -0,0 +1,12 @@
+class Vegetable < ApplicationRecord
+
+  has_one_attached :vegetable_image
+
+  validates :name, presence: true
+  validates :seasonal, presence: true
+  validates :telephone_numberproduction_area, presence: true
+  validates :keeping, presence: true
+  validates :introduction, presence: true
+
+
+end
---
+++ db/migrate/20230925015432_create_vegetables.rb
@@ -0,0 +1,12 @@
+class CreateVegetables < ActiveRecord::Migration[6.1]
+  def change
+    create_table :vegetables do |t|
+      t.string :name, null: false
+      t.string :seasonal, null: false
+      t.text :production_area, null: false
+      t.text :keeping, null: false
+      t.text :introduction, null: false
+      t.timestamps
+    end
+  end
+end

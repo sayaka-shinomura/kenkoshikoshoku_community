72846d7ad79ebab4517fce442555b1235f6848f1
sayaka-shinomura <138592145+sayaka-shinomura@users.noreply.github.com>
2023-10-05 11:25:18 +0000

    [ユーザー:レシピ]登録作成途中

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -9,7 +9,6 @@ class Recipe < ApplicationRecord
   has_one_attached :recipe_image
 
   #難易度評価
-  enum difficulty: { ☆: 1, ☆☆: 2, ☆☆☆: 3, ☆☆☆☆: 4, ☆☆☆☆☆: 5}
-
+  enum difficulty: { ★: 1, ★★: 2, ★★★: 3, ★★★★: 4, ★★★★★: 5}
 
 end

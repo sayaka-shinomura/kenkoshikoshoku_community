3b9e7b5285460c9ccb8199c8a7ef849296b23e10
sayaka-shinomura <138592145+sayaka-shinomura@users.noreply.github.com>
2023-10-22 06:16:17 +0000

    [ユーザー:リクエスト]一覧修正

--- app/helpers/public/homes_helper.rb
+++ app/helpers/public/homes_helper.rb
@@ -1,2 +1,15 @@
 module Public::HomesHelper
+
+  def content_text
+    <<-"EOS".strip_heredoc
+ご意見・ご要望を入力してください。
+頂いたご意見・ご要望は管理者が確認後、対応させていただきます。
+
+リクエスト例
+・〇〇〇のレシピを投稿したいので、野菜一覧に追加してほしい
+・△△△の栄養素情報を充実させてほしい
+    EOS
+  end
+
+
 end

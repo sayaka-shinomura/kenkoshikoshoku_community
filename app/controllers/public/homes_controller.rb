1b3f34c1583f7ed0ad3a6c7e4b691633207efbf9
sayaka-shinomura <138592145+sayaka-shinomura@users.noreply.github.com>
2023-10-30 06:31:12 +0000

    [user:about]作成

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -7,10 +7,12 @@ class Public::HomesController < ApplicationController
 
     @request = Request.new
     @requests = Request.page(params[:page]).per(5).order(created_at: :desc)
+
+    @vegetables = Vegetable.all
   end
 
  def about
-   @vegetable_ranks = Vegetable.find(VegetableTag.group(:vegetable_id).order('count(vegetable_id) desc').limit(10).pluck(:vegetable_id))
+    @vegetable_ranks = Vegetable.find(VegetableTag.group(:vegetable_id).order('count(vegetable_id) desc').limit(10).pluck(:vegetable_id))
     @want_ranks = Recipe.find(WantToMakeList.group(:recipe_id).order('count(recipe_id) desc').limit(5).pluck(:recipe_id))
     @made_ranks = Recipe.find(MadeList.group(:recipe_id).order('count(recipe_id) desc').limit(5).pluck(:recipe_id))
  end

57020355829fc3470cb0accdf6cb95701a148a81
sayaka-shinomura <138592145+sayaka-shinomura@users.noreply.github.com>
2023-12-10 14:08:01 +0000

    [spec/system/signsup]作成途中

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,11 +1,10 @@
 FactoryBot.define do
   factory :user do
-    account_name { 'sara' }
+    account_name { 'sora' }
     email { Faker::Internet.email }
-    telephone_number { '12345678901' }
+    telephone_number { '09000000001' }
     birth_date { '19990101' }
-    gender { '1' }
-    encrypted_password { 'password' }
-
+    gender { '0' }
+    password { 'password' }
   end
 end
--- spec/system/signups_spec.rb
+++ spec/system/signups_spec.rb
@@ -2,3 +2,25 @@
 
 require 'rails_helper'
 
+RSpec.describe "ユーザー新規登録", type: :system do
+  let(:user) { build(:user) }
+
+  describe '正しく登録' do
+    context 'フォームの入力値が正常' do
+      it '正確な情報を入力したら登録可能' do
+        visit new_user_registration_path
+        expect(page).to have_content('新規登録')
+        fill_in 'user_account_name', with: user.account_name
+        fill_in 'user_email', with: user.email
+        fill_in 'user_telephone_number', with: user.telephone_number
+        fill_in 'user_birth_date', with: user.birth_date
+        fill_in 'user_password', with: user.password
+        fill_in 'user_password_confirmation', with: user.password_confirmation
+
+        expect { click_button '新規登録' }.to change(User.all, :count).by(1)
+      end
+    end
+  end
+
+
+end
